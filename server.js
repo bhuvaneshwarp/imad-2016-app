2dfe9023f75f8cc18c6493c574b434b56d1c94e1
bhuvaneshwarp <bhuvaneshwar.bh@gmail.com>
2016-10-01 13:12:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,15 +8,19 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+app.get('/ article-one', function (req, res) {
+  res.send('article one will be serverd');
 });
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+
+app.get('/ article-two', function (req, res) {
+  res.send('article two will be serverd');
 });
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+
+app.get('/ article-three', function (req, res) {
+  res.send('article three will be serverd');
 });
+
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
