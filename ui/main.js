b4a5054c83459eff817942f122d2ef11ab21afc9
bhuvaneshwarp <bhuvaneshwar.bh@gmail.com>
2016-10-02 14:02:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,8 +25,6 @@ button.onclick = function () {
 };
 
 // Submit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
   
